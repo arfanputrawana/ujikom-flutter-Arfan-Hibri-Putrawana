cf103fc56dfe35f9fb456eca7ae357d89e8cfdb6
Arfan Hibri Putrawana <arfan.putrawana@gmail.com>
2026-05-04 13:49:55 +0700

    fix base href for github pages

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -42,7 +42,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ad0cd637e6102dc27752b6b49996cbdd
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "ef19a50245131edb92415e7b275e65fb",
+"flutter_bootstrap.js": "4405b2c4789a9b13491ce4c762f27fae",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
